ee4830b5a1860ff36ebc47e7d4d6e7d52bc59abc
maartenvanderhoef <maarten@doingcloudright.com>
2020-12-18 12:42:52 +0100

    init

---
+++ examples/test/main.tf
@@ -0,0 +1,31 @@
+module "ssm_store" {
+  source = "../../"
+
+  tags =  {
+   "Created by" = "terraform"
+  }
+  kms_alias =  "alias/aws/ssm"
+
+  name_prefix = "/applications/app1"
+  # overwrite = true
+  parameters = [
+  {
+    name      = "secure"
+    value     = "securevalue"
+    type      = "SecureString"
+    overwrite = true
+  },
+  {
+    name      = "secure2"
+    value     = "securevalue2"
+    type      = "SecureString"
+    overwrite = true
+  },
+  {
+    name      = "insecure"
+    value     = "insecurevalue"
+    type      = "String"
+    overwrite = true
+  },
+]
+}
